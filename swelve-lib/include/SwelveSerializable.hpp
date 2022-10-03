acf1bdd15e0aa48a4ccf55c4131c36b42db2fda1
Hendrik A <lightjavacode@gmail.com>
2022-10-03 21:32:05 +0200

    SwelveString

--- swelve-lib/include/SwelveSerializable.hpp
+++ swelve-lib/include/SwelveSerializable.hpp
@@ -3,6 +3,8 @@
 #include "SwelveDeclarations.hpp"
 #include "SwelveUtils.hpp"
 
+#include <string>
+
 namespace swelve
 {
 
@@ -12,6 +14,24 @@ namespace swelve
         virtual size_t GetSerializableSize() = 0;
         virtual void Export(SwelveStream& stream) = 0;
         virtual void Import(SwelveStream& stream) = 0;
+    };
+
+    class SwelveString
+    {
+    public:
+        static constexpr size_t smStringLimitations = 0x200;
+
+        SwelveString(std::string content);
+        SwelveString(const char* content);
+        SwelveString(SwelveStream& stream);
+
+        virtual ~SwelveString();
+
+        virtual size_t GetSerializableSize();
+        virtual bool Export(SwelveStream& stream);
+        virtual bool Import(SwelveStream& stream);
+
+        std::string str;
     };
 
 }
---
+++ swelve-lib/source/SwelveString.cpp
@@ -0,0 +1,50 @@
+#include "SwelveSerializable.hpp"
+
+#include <cstdint>
+
+static_assert(sizeof(size_t) == 0x08);
+
+namespace swelve
+{
+
+    size_t SwelveString::GetSerializableSize()
+    {
+
+    }
+
+    bool SwelveString::Export(SwelveStream& stream)
+    {
+        if(str.length() > smStringLimitations)
+        {
+            return false;
+        }
+
+        stream.Write(str.length());
+        stream.WriteRawBuffer((void*)str.c_str(), str.length());
+
+        return true;
+    }
+
+    bool SwelveString::Import(SwelveStream& stream)
+    {
+        char* content = nullptr;
+        size_t contentSize = 0;
+
+        stream.Read(&contentSize);
+
+        if(contentSize > smStringLimitations)
+        {
+            return false;
+        }
+        
+        content = new char[contentSize + 1];
+        memset(content, 0, contentSize + 1);
+
+        str.append(content);
+
+        delete[] content;
+
+        return true;
+    }
+
+}
